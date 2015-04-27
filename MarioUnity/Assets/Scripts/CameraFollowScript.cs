6d05940fb9a16969418101cc382f93020573bf90
Nils <bognil14@student.westerdals.no>
2015-04-27 20:17:21 +0200

    La til follow camera, sletta drittScript

---
+++ MarioUnity/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+
+	// OverWorld Camera Y position
+	private const float OVERWORLD_Y = 6.66f;
+
+	// Target to follow
+	public GameObject target;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (target.GetComponent<Rigidbody2D> ().position.x,OVERWORLD_Y, -5);
+	}
+}
