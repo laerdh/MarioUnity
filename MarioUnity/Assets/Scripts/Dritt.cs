c3c56288a3314317f74b3a35ce960c0ff1eae47b
Daniel Meinecke <dannemeinecke@gmail.com>
2015-04-27 15:12:17 +0200

    LALALA

---
+++ MarioUnity/Assets/Scripts/Dritt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dritt : MonoBehaviour {
+
+	//Stein er ful
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
