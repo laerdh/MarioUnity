185177ae6ead2a587afaade9c47f33102bcdfa0b
Nils <bognil14@student.westerdals.no>
2015-04-27 21:24:15 +0200

    La til sprinting med litt delay som i orginalen

--- MarioUnity/Assets/Scripts/PlayerMoveScript.cs
+++ MarioUnity/Assets/Scripts/PlayerMoveScript.cs
@@ -3,81 +3,52 @@ using System.Collections;
 
 public class PlayerMoveScript : MonoBehaviour {
 
-	/*
-	Rigidbody player;
-	Animator animator;
-	public float moveSpeedIncrease;
-	public float gravity;
-	public float jumpHeight;
-
-	// Use this for initialization
-	void Start () {
-		player = GetComponent<Rigidbody> ();
-		animator = GetComponent<Animator> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
-
-	// FixedUpdate
-	void FixedUpdate() {
-		float dirX = Input.GetAxis ("Horizontal");
-
-		if (Input.GetKeyUp (KeyCode.LeftArrow) || Input.GetKeyUp (KeyCode.RightArrow)) {
-			dirX = 0;
-		} 
-
-
-		movePlayer (dirX);
-		animatePlayer (dirX);
-		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			player.velocity = new Vector3(0f, jumpHeight, 0f);
-		}
-
-	}
-
-	// move player
-	void movePlayer(float dirX) {
-		player.velocity = new Vector3 (dirX * moveSpeedIncrease, 0f, 0f);
-
-
-	}
-
-	// Animate the player 
-	void animatePlayer(float dirX) {
-		// Checks if the player is running
-		if (dirX != 0) {
-			animator.SetBool ("isRunning", true);
-		} else animator.SetBool ("isRunning", false);
-	}
-	*/
-
 	public float moveSpeed;
 	public float jumpHeight;
+	private float moveSpeedDef;
+	private int sprintDelay = 10;
 
 	private Animator animator;
 
 	void Start() {
 		animator = GetComponent<Animator> ();
+		moveSpeedDef = moveSpeed;
 	}
 
 	void Update() {
 
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
-		}
-		if (Input.GetKey (KeyCode.A)) {
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
-		}
-		if (Input.GetKey (KeyCode.D)) {
-			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
-		}
+		sprint();
+	
+		keyBoardInput ();
 
 		float dirX = Input.GetAxis ("Horizontal");
 		animatePlayer(dirX);
 
+	}
+
+	// Method for checking keyboard input
+	void keyBoardInput() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		}
+	}
+	// Method for making the player sprint
+	void sprint() {
+		if (isSprinting ()) {
+			if(sprintDelay > 0)
+				sprintDelay--;
+			if(sprintDelay <= 0)
+				moveSpeed = 10;
+		} else {
+			moveSpeed = moveSpeedDef;
+			sprintDelay = 10;
+		}
 	}
 
 	// Animate the player 
@@ -93,6 +64,13 @@ public class PlayerMoveScript : MonoBehaviour {
 		} else if (dirX > 0) {
 			GetComponent<Rigidbody2D>().transform.localScale = new Vector3 (6, 6, 0);
 		}
+	}
+
+	// Returns true if player is sprinting 
+	bool isSprinting() {
+		if(Input.GetKey(KeyCode.LeftShift)) {
+			return true;
+		} return false;
 	}
 
 }
