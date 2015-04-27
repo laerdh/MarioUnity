9220287984e7e34c13d6de962ecd78a426d8f43c
Nils <bognil14@student.westerdals.no>
2015-04-27 20:07:00 +0200

    La til hopping

--- MarioUnity/Assets/Scripts/PlayerMoveScript.cs
+++ MarioUnity/Assets/Scripts/PlayerMoveScript.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class PlayerMoveScript : MonoBehaviour {
 
-
+	/*
 	Rigidbody player;
 	Animator animator;
 	public float moveSpeedIncrease;
+	public float gravity;
+	public float jumpHeight;
 
 	// Use this for initialization
 	void Start () {
@@ -25,31 +27,72 @@ public class PlayerMoveScript : MonoBehaviour {
 
 		if (Input.GetKeyUp (KeyCode.LeftArrow) || Input.GetKeyUp (KeyCode.RightArrow)) {
 			dirX = 0;
-		}
+		} 
 
 
 		movePlayer (dirX);
 		animatePlayer (dirX);
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			player.velocity = new Vector3(0f, jumpHeight, 0f);
+		}
 
 	}
-
 
 	// move player
 	void movePlayer(float dirX) {
 		player.velocity = new Vector3 (dirX * moveSpeedIncrease, 0f, 0f);
 
-		if (dirX < 0) {
-			player.transform.localScale = new Vector3 (-6, 6, 0);
-		} else if (dirX > 0) {
-			player.transform.localScale = new Vector3 (6, 6, 0);
-		}
+
 	}
 
+	// Animate the player 
 	void animatePlayer(float dirX) {
+		// Checks if the player is running
+		if (dirX != 0) {
+			animator.SetBool ("isRunning", true);
+		} else animator.SetBool ("isRunning", false);
+	}
+	*/
+
+	public float moveSpeed;
+	public float jumpHeight;
+
+	private Animator animator;
+
+	void Start() {
+		animator = GetComponent<Animator> ();
+	}
+
+	void Update() {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+		}
+
+		float dirX = Input.GetAxis ("Horizontal");
+		animatePlayer(dirX);
+
+	}
+
+	// Animate the player 
+	void animatePlayer(float dirX) {
+		// Checks if the player is running
 		if (dirX != 0) {
 			animator.SetBool ("isRunning", true);
 		} else animator.SetBool ("isRunning", false);
 
-
+		// Checks the direction the player is moving and flips the player
+		if (dirX < 0) {
+			GetComponent<Rigidbody2D>().transform.localScale = new Vector3 (-6, 6, 0);
+		} else if (dirX > 0) {
+			GetComponent<Rigidbody2D>().transform.localScale = new Vector3 (6, 6, 0);
+		}
 	}
+
 }
