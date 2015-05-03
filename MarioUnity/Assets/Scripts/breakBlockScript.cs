3ed26c10842a10378feca8b7df638f6b7dc3bd93
Nils <bognil14@student.westerdals.no>
2015-05-03 17:43:13 +0200

    Kan knuse klossene nå

--- MarioUnity/Assets/Scripts/breakBlockScript.cs
+++ MarioUnity/Assets/Scripts/breakBlockScript.cs
@@ -3,24 +3,25 @@ using System.Collections;
 
 public class breakBlockScript : MonoBehaviour {
 
+	/*
+	 * This script is to make sure that the block is destroyed if th player hits it from the bottom
+	 */
+
 	private bool isHit;
 	private Vector2 pos;
-	public LayerMask player;
+	public LayerMask playerLayer;
+	private Rigidbody2D player;
 
 	void Start() {
-		pos = new Vector2 (transform.position.x, transform.position.y);
+		pos = new Vector2 (transform.position.x, transform.position.y -0.2f);
+		player = GameObject.Find ("Player").GetComponent<Rigidbody2D>();
 	}
 
 	void Update() {
-		isHit = Physics2D.OverlapCircle (pos, 0.5f, player);
+		isHit = Physics2D.OverlapCircle (pos, 0.2f, playerLayer);
 		if(isHit) {
-			print("hit");
+			player.velocity = new Vector2(player.velocity.x, 0f);
 			Destroy(this.gameObject);
 		}
-	}
-
-	void OnTriggerEnter2D(Collider2D other) {
-
-		//Destroy (this.gameObject);
 	}
 }
