b32b649dca9138c96643bd372cbc86ed07597cfc
Nils <bognil14@student.westerdals.no>
2015-05-03 17:09:13 +0200

    kan hoppe i klossene nå og ødelegge dem, må fikse at mario stopper

--- MarioUnity/Assets/Scripts/breakBlockScript.cs
+++ MarioUnity/Assets/Scripts/breakBlockScript.cs
@@ -4,9 +4,19 @@ using System.Collections;
 public class breakBlockScript : MonoBehaviour {
 
 	private bool isHit;
+	private Vector2 pos;
+	public LayerMask player;
+
+	void Start() {
+		pos = new Vector2 (transform.position.x, transform.position.y);
+	}
 
 	void Update() {
-
+		isHit = Physics2D.OverlapCircle (pos, 0.5f, player);
+		if(isHit) {
+			print("hit");
+			Destroy(this.gameObject);
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
