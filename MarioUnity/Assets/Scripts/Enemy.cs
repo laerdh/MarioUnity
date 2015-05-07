0d734a557c8b97e9f3b323d09e04f0303fc0f303
Lars Dahl <laerdh@gmail.com>
2015-05-07 14:42:09 +0200

    Kollisjoner på enemies

--- MarioUnity/Assets/Scripts/Enemy.cs
+++ MarioUnity/Assets/Scripts/Enemy.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 	private Rigidbody2D enemyTurtle;
 	private float speed = -1f;
+	private int direction = 5;
 	// Use this for initialization
 	void Start () 
 	{
@@ -13,13 +14,15 @@ public class Enemy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{	
-		enemyTurtle.transform.localScale = new Vector3 (5, 5, 0);
-		enemyTurtle.transform.Translate (new Vector2(speed, 0) * Time.deltaTime);
+		enemyTurtle.transform.localScale = new Vector3 (direction, 5, 0);
+		enemyTurtle.velocity = new Vector2(speed, 0);
 	}
 
-	void OnTriggerEnter(Collider other) {
+	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "pipe") {
 			Debug.Log ("Test");
+			speed = -speed;
+			direction = -direction;
 		}
 	}
 }
