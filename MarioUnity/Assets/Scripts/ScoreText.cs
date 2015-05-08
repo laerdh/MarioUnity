0c6af5c620d5b256ba5e21ac5009d006edd4c9fe
Daniel Meinecke <dannemeinecke@gmail.com>
2015-05-08 09:32:02 +0200

    Added Scene "StartScreen", and some textures

---
+++ MarioUnity/Assets/Scripts/ScoreText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreText : MonoBehaviour {
+
+	public GUIText scoreText;
+	public int score;
+
+
+	// Use this for initialization
+	void UpdateScore () {
+		scoreText.text = "Score " + score;
+	}
+}
