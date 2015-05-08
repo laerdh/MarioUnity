faf6773dfb9e5169569ba69a442326aa7c97c93c
Lars Dahl <laerdh@gmail.com>
2015-05-08 14:50:27 +0200

    Endring

--- MarioUnity/Assets/EnemyMove.cs
+++ MarioUnity/Assets/EnemyMove.cs
@@ -9,29 +9,30 @@ public class EnemyMove : MonoBehaviour {
 	public bool colliding;
 	public LayerMask detectObject;
 	public Transform weakness;
+	private bool sweepMode;
 	private int hit = 0;
-	private bool dying;
 	public Animator animator;
-	public bool collision;
 
 	// Use this for initialization
 	void Start () {
-
 		enemy = GetComponent<Rigidbody2D> ();
 		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		enemy.velocity = new Vector2 (velocity, enemy.velocity.y);
+		enemy.velocity = new Vector2 (velocity, 0);
 
 		colliding = Physics2D.Linecast (sightStart.position, sightEnd.position, detectObject);
 
 		if (colliding) {
 			transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
-			velocity = velocity * -1;
+			velocity *= -1;
 		}
 
+		if (sweepMode) {
+			velocity = 10;
+		}
 	}
 
 	void OnDrawGizmos() {
@@ -44,15 +45,20 @@ public class EnemyMove : MonoBehaviour {
 		if (other.gameObject.name == "Player") {
 			if (this.gameObject.tag == "EnemyTurtle") {
 				hit++;
-				animator.SetBool ("isHit", true);
 				velocity = 0;
-				if (hit == 2) {
-					enemy.AddForce (new Vector2(100, 600));
+
+				if (hit % 2 == 0) {
+					sweepMode = true;
+				} else {
+					sweepMode = false;
 				}
+				animator.SetBool ("isHit", true);
+				// More speed when hitted the second time.
 			}
 
 			if (this.gameObject.tag == "EnemyGoomba") {
 				animator.SetBool ("isHit", true);
+				StartCoroutine (delayAction (5));
 				Dies ();
 			}
 			//float height = other.contacts[0].point.y - weakness.position.y;
@@ -62,7 +68,10 @@ public class EnemyMove : MonoBehaviour {
 	}
 
 	void Dies() {
-		dying = true;
 		Destroy (this.gameObject);
+	}
+
+	IEnumerator delayAction(int time) {
+		yield return new WaitForSeconds(time);
 	}
 }
