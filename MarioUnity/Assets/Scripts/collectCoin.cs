5f896972555d08ce9e3d3e4ef8947f243d5e5249
Nils <bognil14@student.westerdals.no>
2015-05-09 00:02:49 +0200

    La til så man kan samle mynter og at det oppdateres i scoreManager

---
+++ MarioUnity/Assets/Scripts/collectCoin.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collectCoin : MonoBehaviour {
+
+	public ScoreManager scoreManager;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "coin") {
+			//print ("traff en mynt");
+			Destroy(other.gameObject);
+			scoreManager.addCoin();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
