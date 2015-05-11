1e5f39881f4310be6805a6180ec2242593796e5f
Nils <bognil14@student.westerdals.no>
2015-05-11 10:50:24 +0200

    Blokkene spawner mynter når de blir truffet nå

---
+++ MarioUnity/Assets/Scripts/coinScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coinScript : MonoBehaviour {
+
+	// To decide if the coin is static or not. 1 is static, 0 is not.s
+	public bool isMovable;
+
+	private int timer = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isMovable) {
+			timer--;
+			if (timer <= 0) {
+				Destroy (this.gameObject);
+			}
+		}
+	}
+}
