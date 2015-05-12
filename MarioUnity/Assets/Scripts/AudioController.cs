bb5f1106b84206bdf8c675f079be49a0b7b52e43
Daniel Meinecke <dannemeinecke@gmail.com>
2015-05-12 14:38:59 +0200

    Endring i script audiocontroller

--- MarioUnity/Assets/Scripts/AudioController.cs
+++ MarioUnity/Assets/Scripts/AudioController.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class AudioController : MonoBehaviour {
 
+	AudioSource smallJump;
+	AudioSource bigJump;
+
+	void Start(){
+		AudioSource[] audios = GetComponents<AudioSource> ();
+		smallJump = audios [0];
+		bigJump = audios [1];
+	}
 
 	void playSmallMarioJump(){
-
+		smallJump.Play ();
 	}
 
 	void playBigMarioJump(){
-
+		bigJump.Play ();
 	}
 }
