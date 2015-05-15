a3fcb90bb174cc6f122ebaeb275c90beda8478ef
Nils <bognil14@student.westerdals.no>
2015-05-15 02:09:23 +0200

    no message

--- MarioUnity/Assets/Scripts/EnemyKoopa.cs
+++ MarioUnity/Assets/Scripts/EnemyKoopa.cs
@@ -5,7 +5,7 @@ public class EnemyKoopa : MonoBehaviour {
 	public PlayerMoveScript mario;
 	private Animator anim;
 	public Rigidbody2D enemy;
-	public float velocity = -1f;
+	public float moveSpeed = -1f;
 	public Transform sightStart;
 	public Transform sightEnd;
 	public LayerMask detectObject;
@@ -15,6 +15,9 @@ public class EnemyKoopa : MonoBehaviour {
 
 	private GameObject thePlayer;
 	private bool isAwake;
+	private bool started = false;
+
+	private int lives = 2;
 
 	// Use this for initialization
 	void Start () 
@@ -22,6 +25,7 @@ public class EnemyKoopa : MonoBehaviour {
 		enemy = GetComponent<Rigidbody2D> ();
 		anim = GetComponent<Animator> ();
 		thePlayer = GameObject.Find ("Player");
+		mario = thePlayer.GetComponent<PlayerMoveScript> ();
 	}
 	
 	// Update is called once per frame
@@ -33,13 +37,17 @@ public class EnemyKoopa : MonoBehaviour {
 			isAwake = true;
 
 		if (isAwake) {
-			enemy.velocity = new Vector2 (velocity, enemy.velocity.y);
+			if(!started) {
+				enemy.velocity = new Vector2 (moveSpeed, enemy.velocity.y);
+				started = true;
+			}
+			enemy.velocity = new Vector2 (moveSpeed, enemy.velocity.y);
 		
 			colliding = Physics2D.Linecast (sightStart.position, sightEnd.position, detectObject);
 		
 			if (colliding) {
 				transform.localScale = new Vector2 (transform.localScale.x * -1, transform.localScale.y);
-				velocity *= -1;
+				moveSpeed *= -1;
 			}
 		}
 	}
@@ -52,30 +60,58 @@ public class EnemyKoopa : MonoBehaviour {
 	
 	void OnCollisionEnter2D(Collision2D other) 
 	{
-		if (other.gameObject.tag == "Player") 
-		{
-			bool isSuper = mario.getHasSuperStar();
-			if(!isSuper) {
-				float height = other.contacts[0].point.y - weakness.position.y;
+		Debug.Log (other.gameObject.tag);
+		if (other.gameObject.tag == "Player") {
+			bool isSuper = mario.getHasSuperStar ();
+			if (isSuper) {
+				Dies ();
+			} else if (lives == 2) {
+				if (!isSuper) {
+					float height = other.contacts [0].point.y - weakness.position.y;
 			
-				if (height > 0) 
-				{
-					Dies();
-					other.rigidbody.AddForce(new Vector2 (0, 300));
-				} else 
-				{
-				mario.removeLife();
+					if (height > 0) {
+						//Dies();
+						lives--;
+						if (lives == 0)
+							Dies ();
+
+						if (lives == 1) {
+							enemy.velocity = new Vector2 (0, 0);
+							anim.SetBool ("isHit", true);
+						}
+
+						other.rigidbody.AddForce (new Vector2 (0, 300));
+					} else {
+						mario.removeLife ();
+					}
 				}
-			}	
-			else if(isSuper) {
-				Dies();
+			} else if (lives == 1) {
+				if (moveSpeed > 4 || moveSpeed < -4) {
+				//	mario.removeLife ();
+				}
+
+				float distance = transform.position.x - other.transform.position.x;
+				if (distance >= 0)
+					moveSpeed = 5;//enemy.velocity = new Vector2(7,enemy.velocity.y);
+				else if (distance < 0)
+					moveSpeed = -5;//enemy.velocity = new Vector2(-7,enemy.velocity.y);
+				other.rigidbody.AddForce (new Vector2 (0, 300));
 			}
+
+		} else if (other.gameObject.tag == "deadly" && lives == 1) {
+			if (moveSpeed > 4 || moveSpeed < -4) {
+				EnemyKill k = other.gameObject.GetComponent<EnemyKill> ();
+				if (k != null)
+					k.gotShot ();
+			}
+		} else if(other.gameObject.tag == "pipe" ) {
+			moveSpeed = moveSpeed * -1;
 		}
 	}
 	
 	void Dies() 
 	{
-		velocity = 0;
+		moveSpeed = 0;
 		anim.SetBool ("isHit", true);
 		Destroy (this.gameObject, 0.5f);
 		gameObject.tag = "neutralized";
